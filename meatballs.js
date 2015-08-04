8af9436abb665534a560a636ff89f60afe6ab2db
noelleantoinette <noelleantoinette@gmail.com>
2015-08-04 14:06:48 -0400

    working first two tests

--- meatballs.js
+++ meatballs.js
@@ -1,13 +1,22 @@
-var fs = require('fs');
+var fs = require('fs'),
+    counter = 0,
+    content;
 
 module.exports = {
 
-    meatballs: function() {
-        // return 'hello'
-        fs.readFile('./meatballs.txt', function(err, data) {
+    // logger: function(ans) {
+    //     return ans;
+    // },
+
+    meatballs: function(cb) {
+
+        fs.readFile('./meatballs.txt', 'utf-8', function(err, data) {
+            counter += 1;
             if (err) throw err;
-           console.log(data)
-            
+            content = data.toString();
+            cb(content);
         })
+        
+        
     }
 }
--- meatballsSpec.js
+++ meatballsSpec.js
@@ -1,15 +1,16 @@
 var expect = require('chai').expect,
 	source = require('./meatballs.js');
-
 	
 describe('Bills first movie', function () {
-
-	it('says hello', function (){
-		expect(source.meatballs()).to.equal('hello');
+	
+	it('has a length of 8538', function (){
+		source.meatballs(function(content){
+			expect(content).to.equal('Meatballs (film)')
+		})
 	});
 
 	it('has a description', function (){
 		expect(typeof source.meatballs).to.equal('function');
 	});
-})
 
+})
